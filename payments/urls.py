46ec321699af9911b19242fdb96cca5aad9e3086
henil suchak <henilsuchak8311@gmail.com>
2025-07-21 07:36:24 +0530

    Configure Razorpay and production settings for deployment

--- payments/urls.py
+++ payments/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 app_name = "payments"
 
@@ -6,4 +6,5 @@ urlpatterns = [
     path('initiate-payment/', views.initiate_payment, name='initiate_payment'),
     path('payment-success/', views.handle_payment_success, name='handle_payment_success'),
     path('payment-failure/', views.handle_payment_failure, name='handle_payment_failure'),
+    path('payments/', include(('payments.urls', 'payments'), namespace='payments')),
 ]
